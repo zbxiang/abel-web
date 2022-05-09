b34f8be98a6dde203debc799d024e14b509fb8f1
zbxiang <1144653975@qq.com>
2022-05-09 22:06:10 +0800

    封装storage

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,9 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/theme-chalk/index.css'
-import config from './config'
+import http from './utils/http'
 
 console.log("环境变量=>", import.meta.env)
-console.log(config.baseApi)
 const app = createApp(App)
-
+app.config.globalProperties.$http = http
 app.use(router).use(ElementPlus).mount('#app')
