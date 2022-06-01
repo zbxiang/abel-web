85c0549f0a99acc2fc3aa85aa871dc5db805ebe6
zhangbaoxiang001 <1144653975@qq.com>
2022-06-01 17:38:50 +0800

    路由守卫

--- src/admin/main.ts
+++ src/admin/main.ts
@@ -9,15 +9,41 @@ import storage from '@C/utils/storage'
 import services from '@C/services'
 import mitt from 'mitt'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import { initial } from 'lodash'
 const emitter = mitt();
 
 console.log("环境变量=>", import.meta.env)
-const app = createApp(App)
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-app.component(key, component)
+
+let whiteList = ['login']
+
+router.beforeEach(async (to, from, next) => {
+    // if (whiteList.includes(to.path)) {
+    //     next()
+    //     return
+    // }
+    // if (to.path !== from.path) {
+    //     return
+    // }
+    next()
+})
+
+// 获取目录
+// if (location.hash !== '#/login') {
+//     init()
+// }
+
+init()
+
+async function init() {
+    const app = createApp(App)
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+        app.component(key, component)
+    }
+    app.config.globalProperties.$http = http
+    app.config.globalProperties.$services = services
+    app.config.globalProperties.$storage = storage
+    app.config.globalProperties.emitter = emitter
+    app.use(router).use(store).use(ElementPlus).mount('#app')
 }
-app.config.globalProperties.$http = http
-app.config.globalProperties.$services = services
-app.config.globalProperties.$storage = storage
-app.config.globalProperties.emitter = emitter
-app.use(router).use(store).use(ElementPlus).mount('#app')
+
+
