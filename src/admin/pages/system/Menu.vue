5bf6c60e1b89bb963a675370d3bf7a9f2ccc9942
zbxiang <1144653975@qq.com>
2022-06-06 23:15:37 +0800

    菜单管理

---
+++ src/admin/pages/system/Menu.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="user-manager">
+        <div class="query-form">
+            <el-form ref="form" :inline="true" :model="queryForm">
+                
+            </el-form>
+        </div>
+    </div>
+</template>
